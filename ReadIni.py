b27e354ac6f591aeb0253159e634ba8bf7bd45ff
AlphaDragon601 <alphadragon601@gmail.com>
2023-04-23 20:12:16 -0400

    Read specific data from ini based on arg

--- ReadIni.py
+++ ReadIni.py
@@ -1,17 +1,32 @@
 import configparser
-import sys
+import sys  
 config = configparser.ConfigParser()
 
 configFile = sys.argv[1]
 
+PrgmSection = sys.argv[2]
+RequestedVal = sys.argv[3]
 config.read(configFile)
 
+
+if RequestedVal == "v":
+    print(config[PrgmSection]["version"])
+elif RequestedVal == "a":
+    print(config[PrgmSection]["architecture"])
+elif RequestedVal == "m":
+    print(config[PrgmSection]["maintainer"])
+elif RequestedVal == "d":
+    print(config[PrgmSection]["depends"])
+elif RequestedVal == "u":
+    print(config[PrgmSection]["URL"])
+
+
 # print(config.sections())
-for section in config.sections():
-    print(section)
-    print("\t" + config[section]["version"])
-    print("\t" + config[section]["architecture"])
-    print("\t" + config[section]["maintainer"])
-    print("\t" + config[section]["depends"])
-    print("\n*********\n")
+# for section in config.sections():
+#     print(section)
+#     print("\t" + config[section]["version"])
+#     print("\t" + config[section]["architecture"])
+#     print("\t" + config[section]["maintainer"])
+#     print("\t" + config[section]["depends"])
+#     print("\n*********\n")
 
