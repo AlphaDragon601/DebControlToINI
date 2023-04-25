a6be91f4e9d079c8410e39776f38afda5db20238
AlphaDragon601 <alphadragon601@gmail.com>
2023-04-24 21:23:28 -0400

    fixed MASSIVE bug

--- RemovePkgFromIni.py
+++ RemovePkgFromIni.py
@@ -5,6 +5,10 @@ config = configparser.ConfigParser()
 IniFile = sys.argv[1]
 PkgToRemove = sys.argv[2]
 
+
+config.read(IniFile)
+
+
 config.remove_section(PkgToRemove)
 
 with open(IniFile, "w") as configFile:
