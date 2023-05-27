72c254f254561e1ca528de36440da82239ba9631
AlphaDragon601 <alphadragon601@gmail.com>
2023-05-27 00:41:36 -0400

    better ReadIni error handling

--- ReadIni.py
+++ ReadIni.py
@@ -17,23 +17,36 @@ if RequestedVal == "l":
     
 # check to make sure that program is in the config
 try:
-    print(config[PrgmSection])
+    print(str(config[PrgmSection]).replace("<Section: " + PrgmSection + ">", "")) # use replace to make this print not actually print
 except:
     print("Unable to find program: " + PrgmSection + " ,is it installed?")
     sys.exit()
 
-if RequestedVal == "v":
-    print(config[PrgmSection]["version"])
-elif RequestedVal == "a":
-    print(config[PrgmSection]["architecture"])
-elif RequestedVal == "m":
-    print(config[PrgmSection]["maintainer"])
-elif RequestedVal == "d":
-    print(config[PrgmSection]["depends"])
-elif RequestedVal == "u":
-    try:
+try:
+
+    if RequestedVal == "v":
+        print(config[PrgmSection]["version"])
+    elif RequestedVal == "a":
+        print(config[PrgmSection]["architecture"])
+    elif RequestedVal == "m":
+        print(config[PrgmSection]["maintainer"])
+    elif RequestedVal == "d":
+        print(config[PrgmSection]["depends"])
+    elif RequestedVal == "u":
         print(config[PrgmSection]["URL"])
-    except:
+    elif RequestedVal == "de":
+        print(config[PrgmSection]["description"])
+
+except:
+    if RequestedVal == "v":
+        print("no version")
+    elif RequestedVal == "a":
+        print("no architecture")
+    elif RequestedVal == "m":
+        print("no maintainer")
+    elif RequestedVal == "d":
+        print("no depends")
+    elif RequestedVal == "u":
         print("no url")
-elif RequestedVal == "de":
-    print(config[PrgmSection]["description"])
+    elif RequestedVal == "de":
+        print("no description")
